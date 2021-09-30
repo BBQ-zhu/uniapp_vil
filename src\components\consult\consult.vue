c202225ecf4f835480f246a7a7223455fb49c999
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-09-30 18:09:44 +0800

    Create src\components\consult\consult.vue

---
+++ src\components\consult\consult.vue
@@ -0,0 +1,155 @@
+<template>
+  <view class="demand">
+    <view class="con-title">专家顾问一对一为您定制方案</view>
+    <view class="con-serve">
+      <span class="colorYellow">2000名</span>专业顾问已为
+      <span class="colorYellow">100万+</span>的企业/用户提供服务
+    </view>
+    <view class="flex mt15">
+      <view class="con-Btn colorYellow">+注册公司</view>
+      <view class="con-Btn colorYellow">+记账</view>
+      <view class="con-Btn colorYellow">+注销</view>
+      <view class="con-Btn colorYellow">+商标</view>
+      <view class="con-Btn colorYellow">+更多服务</view>
+    </view>
+    <view>
+      <u-form :model="form" ref="uForm" label-width="100rpx">
+        <u-form-item
+          label="我需求"
+          prop="title"
+          :leftIconStyle="{color: '#EB6618', fontSize: '26rpx'}"
+          left-icon="file-text"
+          label-width="140"
+        >
+          <u-input v-model="form.title" placeholder="请输入我需求" />
+        </u-form-item>
+        <u-form-item
+          label="手机号"
+          prop="phone"
+          :leftIconStyle="{color: '#EB6618', fontSize: '26rpx'}"
+          left-icon="phone"
+          label-width="140"
+        >
+          <u-input v-model="form.phone" type="number" placeholder="请输入手机号" />
+        </u-form-item>
+        <u-button class="mt15 yellowBtn border50" type="warning" shape="square" @click="submit">立即咨询</u-button>
+      </u-form>
+      <view class="flex flexAround mt10">
+        <view>
+          <u-icon name="integral-fill" size="26" class="colorGreen mr5"></u-icon>信息安全
+        </view>
+        <view>
+          <u-icon name="heart-fill" size="26" class="colorGreen mr5"></u-icon>官方服务
+        </view>
+        <view>
+          <u-icon name="bell-fill" size="26" class="colorGreen mr5"></u-icon>服务透明
+        </view>
+      </view>
+    </view>
+    <u-toast ref="uToast"></u-toast>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        title: '',
+        phone: ''
+      }
+      // rules: {
+      //   title: [{
+      //     required: true,
+      //     message: '请输入需求',
+      //     trigger: ['change', 'blur']
+      //   }],
+      //   phone: [
+      //     {
+      //       required: true,
+      //       message: '请输入手机号',
+      //       trigger: ['change', 'blur']
+      //     },
+      //     {
+      //       validator: (rule, value, callback) => {
+      //         return this.$u.test.mobile(value)
+      //       },
+      //       message: '请输入正确手机号',
+      //       trigger: ['change', 'blur']
+      //     }
+      //   ]
+      // }
+    }
+  },
+  // mounted() {
+  // 	this.$refs.uForm.setRules(this.rules);
+  // },
+  methods: {
+    submit() {
+      //提交
+      if (!this.form.title || !this.form.phone) {
+        this.$refs.uToast.show({
+          title: '请输入信息'
+        })
+        return
+      }
+      if (!/^1[3|4|5|7|8]\d{9}$/.test(this.form.phone)) {
+        this.$refs.uToast.show({
+          title: '请输入正确手机号'
+        })
+        return
+      }
+      let obj = {
+        type: '专业顾问', // 分类
+        proname: this.form.title, // 产品名称
+        name: '移动端客户', // 姓名
+        phone: this.form.phone, // 电话
+        remarks: ''
+      }
+      this.$axios.post(this.$api.createIntegrate, obj).then(res => {
+        if (res.code == 200) {
+          this.$refs.uToast.show({
+            title: '提交成功',
+            type: 'success'
+          })
+          this.form = {
+            title: '',
+            phone: ''
+          }
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.demand {
+  width: 100%;
+  height: auto;
+  padding: 20px 18px;
+  background: #ffffff;
+  border-radius: 8px;
+  text-align: center;
+}
+.con-title {
+  font-size: 18px;
+  font-weight: 800;
+}
+.con-serve {
+  font-size: 12px;
+  padding-top: 10px;
+}
+.con-Btn {
+  width: auto;
+  height: 20px;
+  line-height: 20px;
+  box-sizing: border-box;
+  padding: 0 7px;
+  text-align: center;
+  background: #fff4e6;
+  border-radius: 20px;
+  font-size: 12px;
+  margin-right: 5px;
+}
+</style>
