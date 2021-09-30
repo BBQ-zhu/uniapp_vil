431a3a1df569140701df33ff1668497ab9660638
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-09-30 18:24:18 +0800

    Create src\App.vue

---
+++ src\App.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+	onLaunch: function() {
+		console.log('App Launch');
+		// 拨打电话按钮触发
+		uni.onTabBarMidButtonTap(()=>{
+			var a = document.createElement('a');
+			a.setAttribute('href', 'tel:4001109939');
+			document.body.appendChild(a);
+			a.click();
+		})
+	},
+	onShow: function() {
+		console.log('App Show');
+	},
+	onHide: function() {
+		console.log('App Hide');
+	}
+};
+</script>
+
+<style lang="scss">
+@import 'uview-ui/index.scss';
+@import './common/css/common.css';
+@import './common/css/release.css';
+
+/* 解决头条小程序组件内引入字体不生效的问题 */
+/* #ifdef MP-TOUTIAO */
+@font-face {
+	font-family: uniicons;
+	src: url('/static/uni.ttf');
+}
+/* #endif */
+</style>
