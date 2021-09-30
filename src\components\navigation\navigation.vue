8328b60d5da9fa78241e4728e04b31d5b7321051
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-09-30 18:10:47 +0800

    Create src\components\navigation\navigation.vue

---
+++ src\components\navigation\navigation.vue
@@ -0,0 +1,36 @@
+<template>
+  <view class="content flex flexBetween">
+    <view class="navItem" v-for="(item,index) in navList" :key="index+'nav'" @click="navChange(item.path,'type')">
+      <u-image :src="item.image" width="60rpx" height="60rpx" shape="square" style="margin:0 auto;"></u-image>
+      <view>{{item.title}}</view>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  props: ['navList'],
+  data() {
+    return {}
+  },
+  methods: {
+    navChange(path,fuzz) {
+      uni.navigateTo({
+        url: '/pages/findProdect/findProdect?name=' + path + '&fuzz=' + fuzz
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.content {
+  margin-top: 50px;
+}
+.navItem{
+  width:20%;
+  margin-bottom:20px;
+  text-align: center;
+  line-height: 20px;
+}
+</style>
