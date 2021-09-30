01245e3e3b705f928e2144674fdde95998386b85
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-09-30 18:10:17 +0800

    Create src\components\jm-watermark\jm-watermark.vue

---
+++ src\components\jm-watermark\jm-watermark.vue
@@ -0,0 +1,41 @@
+<template>
+	<view>
+		<view class="copyRightBox">
+			<view class="font" ref='sy' v-for="(item,index) in 36" :key='index'>
+				铸力
+			</view>
+		</view>
+	</view>
+</template>
+<script>
+	export default {
+		data() {
+			return {};
+		},
+		// props: {
+		// 	//载入的标签数据
+		// 	sytext: String
+		// },
+		mounted() {}
+	}
+</script>
+<style>
+	.copyRightBox {
+		overflow: hidden;
+		width: 100%;
+		height: calc(100vh);
+		pointer-events: none;
+		position: fixed;
+		top: -100upx;
+		left:0;
+		z-index: 1;
+	}
+	.font {
+		float: left;
+		transform: rotate(-30deg);
+		margin-bottom: 150upx;
+		margin-left: 100upx;
+		font-size: 30upx;
+		color: rgba(100, 0, 0, 0.1);
+	}
+</style>
