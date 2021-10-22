a9e799bf35ad83ced002d6a9a23bb60ac5c6d5ed
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-10-22 16:07:25 +0800

    Update src\main.js

--- src\main.js
+++ src\main.js
@@ -2,17 +2,21 @@ import Vue from 'vue'
 import App from './App'
 import uView from "uview-ui";
 import commonJS from "@/common/js/common";
-import {get,post} from "@/common/js/http";
-// import {inGet,inPost} from "@/common/js/inHttp";
+import {
+  get,
+  post
+} from "@/common/js/http";
 import api from "@/common/js/api";
 
-// import request from "@/common/js/request";
-// Vue.prototype.$http = request   // this.$http.axios(url,'post',data) 封装的uni.request
 
-Vue.prototype.$URL = 'http://localhost:3030'
+Vue.prototype.$URL = 'http://localhost:3030' //测试后端接口
+// Vue.prototype.$URL = 'http://zlc.zhulif.com:3030' //线上接口地址
+
 Vue.prototype.$commonJS = commonJS
-Vue.prototype.$axios = {get,post} //封装的axios
-// Vue.prototype.$inAxios = {inGet,inPost} //封装的内部axios
+Vue.prototype.$axios = {
+  get,
+  post
+} //封装的axios
 Vue.prototype.$api = api
 Vue.use(uView);
 
@@ -21,7 +25,6 @@ Vue.config.productionTip = false
 App.mpType = 'app'
 
 const app = new Vue({
-	...App
+  ...App
 })
 app.$mount()
-
