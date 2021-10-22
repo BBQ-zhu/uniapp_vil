2993865a8865a5d41df6ccaa7fb274bc3b711bc6
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-10-22 16:01:07 +0800

    Update src\components\navigation\navigation.vue

--- src\components\navigation\navigation.vue
+++ src\components\navigation\navigation.vue
@@ -1,35 +1,46 @@
 <template>
-  <view class="content flex flexBetween">
-    <view class="navItem" v-for="(item,index) in navList" :key="index+'nav'" @click="navChange(item.path,'type')">
-      <u-image :src="item.image" width="60rpx" height="60rpx" shape="square" style="margin:0 auto;"></u-image>
-      <view>{{item.title}}</view>
+  <view class="content flex flexStart">
+    <view
+      class="navItem"
+      v-for="(item, index) in navList"
+      :key="index + 'nav'"
+      @click="navChange(item.path, 'type')"
+    >
+      <u-image 
+        :src="item.image"
+        width="60rpx"
+        height="60rpx"
+        shape="square"
+        style="margin: 0 auto"
+      ></u-image>
+      <view>{{ item.title }}</view>
     </view>
   </view>
 </template>
 
 <script>
 export default {
-  props: ['navList'],
+  props: ["navList"],
   data() {
-    return {}
+    return {};
   },
   methods: {
-    navChange(path,fuzz) {
+    navChange(path, fuzz) {
       uni.navigateTo({
-        url: '/pages/findProdect/findProdect?name=' + path + '&fuzz=' + fuzz
-      })
-    }
-  }
-}
+        url: "/pages/findProdect/findProdect?name=" + path + "&fuzz=" + fuzz,
+      });
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .content {
   margin-top: 50px;
 }
-.navItem{
-  width:20%;
-  margin-bottom:20px;
+.navItem {
+  width: 20%;
+  margin-bottom: 20px;
   text-align: center;
   line-height: 20px;
 }
