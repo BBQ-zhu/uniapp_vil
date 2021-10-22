9517cfe3f916244f35ac03afa86d9713b56c67f8
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-10-22 15:59:41 +0800

    Update src\common\js\http.js

--- src\common\js\http.js
+++ src\common\js\http.js
@@ -4,86 +4,86 @@ import QS from 'qs';
 const baseURL = '/api'
 // 环境的切换
 if (process.env.NODE_ENV == 'development') {
-  axios.defaults.baseURL = baseURL
+    axios.defaults.baseURL = baseURL
 } else if (process.env.NODE_ENV == 'debug') {
-  axios.defaults.baseURL = baseURL
+    axios.defaults.baseURL = baseURL
 } else if (process.env.NODE_ENV == 'production') {
-  axios.defaults.baseURL = baseURL
+    axios.defaults.baseURL = baseURL
 }
 
 //设置请求超时时间
-axios.defaults.timeout = 10000;
+axios.defaults.timeout = 30000;
 
 //给post设置请求头
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
 
 //请求拦截器：校验用户token是否有效
 axios.interceptors.request.use((config) => {
-  if (uni.getStorageSync('vipUserInfo')) {
-    let userInfo = uni.getStorageSync('vipUserInfo')
-    config.headers['Authorization'] = userInfo.token
-  }
-  return config
+    if (uni.getStorageSync('vipUserInfo')) {
+        let userInfo = uni.getStorageSync('vipUserInfo')
+        config.headers['Authorization'] = userInfo.token
+    }
+    return config
 })
 
 //响应拦截器：校验用户token是否有效
 axios.interceptors.response.use((success) => {
-  if (success.status && success.status == 200) {
-    if (success.data.code == 500 || success.data.code == 401 || success.data.code == 403) {
-      uni.showToast({
-        title: success.data.msg,
-        icon: 'none'
-      })
-      return;
+    if (success.status && success.status == 200) {
+        if (success.data.code == 500 || success.data.code == 401 || success.data.code == 403) {
+            uni.showToast({
+                title: success.data.msg,
+                icon: 'none'
+            })
+            return;
+        }
     }
-  }
-  return success
+    return success
 }, (error) => {
-  if (error.response.status == 500 || error.response.status == 504 || error.response.status == 404) {
-    uni.showToast({
-      title: error.response.data.message,
-      icon: 'none'
-    })
-  } else if (error.response.status == 401) {
-    uni.navigateTo({
-      url: '/pages/vipLogin/vipLogin'
-    })
-  } else {
-    if (error.response.data) {
-      uni.showToast({
-        title: error.response.data,
-        icon: 'none'
-      })
+    if (error.response.status == 500 || error.response.status == 504 || error.response.status == 404) {
+        uni.showToast({
+            title: error.response.data.message,
+            icon: 'none'
+        })
+    } else if (error.response.status == 401) {
+        uni.navigateTo({
+            url: '/pages/vipLogin/vipLogin'
+        })
     } else {
-      uni.showToast({
-        title: '未知错误！',
-        icon: 'none'
-      })
+        if (error.response.data) {
+            uni.showToast({
+                title: error.response.data,
+                icon: 'none'
+            })
+        } else {
+            uni.showToast({
+                title: '未知错误！',
+                icon: 'none'
+            })
+        }
     }
-  }
-  return;
+    return;
 })
 
 export function get(url, params) {
-  return new Promise((resolve, reject) => {
-    axios.get(url, {
-      params: params || {}
-    }).then(res => {
-      resolve(res.data);
-    }).catch(err => {
-      reject(err.data)
+    return new Promise((resolve, reject) => {
+        axios.get(url, {
+            params: params || {}
+        }).then(res => {
+            resolve(res.data);
+        }).catch(err => {
+            reject(err.data)
+        })
     })
-  })
 }
 
 export function post(url, params) {
-  return new Promise((resolve, reject) => {
-    axios.post(url, QS.stringify(params || {}))
-      .then(res => {
-        resolve(res.data);
-      })
-      .catch(err => {
-        reject(err.data)
-      })
-  });
+    return new Promise((resolve, reject) => {
+        axios.post(url, QS.stringify(params || {}))
+            .then(res => {
+                resolve(res.data);
+            })
+            .catch(err => {
+                reject(err.data)
+            })
+    });
 }
