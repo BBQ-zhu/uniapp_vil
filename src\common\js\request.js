3431a15d5555e52c8cc9a7e4cdc7f6d17eaed749
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-11-13 20:50:29 +0800

    Update src\common\js\request.js

--- src\common\js\request.js
+++ src\common\js\request.js
@@ -1,54 +1,55 @@
 import {
-  Promise
+    Promise
 } from "core-js";
-const baseUrl = "http://localhost:3030/api/" // 本地地址
-// const baseUrl="http://localhost:3030/api" // 线上地址
+// const baseUrl = "http://localhost:3030/api/" // 本地地址
+
+const baseUrl="http://zlc.zhulif.com/api" // 线上地址
 
 const axios = (url, method, data) => {
-  let meth = method.toUpperCase() //请求头大写
-  if (!data) {
-    data = {}
-  }
+    let meth = method.toUpperCase() //请求头大写
+    if (!data) {
+        data = {}
+    }
 
 
 
-  let reqOpts = {
-    url: baseUrl + url,
-    data,
-    method: meth,
-    header: {
-      'X-Requested-With': 'XMLHttpRequest',
-      'content-type': meth == 'POST' ? 'application/x-www-form-urlencoded' : 'application/json',
-    },
-    dataType: 'json'
-  }
-  reqOpts.header['Authorization'] = (uni.getStorageSync('vipUserInfo') || {}).token || ''
+    let reqOpts = {
+        url: baseUrl + url,
+        data,
+        method: meth,
+        header: {
+            'X-Requested-With': 'XMLHttpRequest',
+            'content-type': meth == 'POST' ? 'application/x-www-form-urlencoded' : 'application/json',
+        },
+        dataType: 'json'
+    }
+    reqOpts.header['Authorization'] = (uni.getStorageSync('vipUserInfo') || {}).token || ''
 
 
-  return new Promise((resolve, reject) => {
-    uni.request(reqOpts).then(res => {
-      console.log(res)
-      if (res[1].data.code == 200) {
-        resolve(res[1].data)
-      } else {
-        console.log(res)
-        uni.showToast({
-          title: res[1].data.msg,
-          icon: 'none'
+    return new Promise((resolve, reject) => {
+        uni.request(reqOpts).then(res => {
+            console.log(res)
+            if (res[1].data.code == 200) {
+                resolve(res[1].data)
+            } else {
+                console.log(res)
+                uni.showToast({
+                    title: res[1].data.msg,
+                    icon: 'none'
+                })
+            }
+        }).catch(error => {
+            console.log(error.code)
+            uni.showToast({
+                title: error,
+                icon: 'error'
+            })
+            reject(error)
         })
-      }
-    }).catch(error => {
-      console.log(error.code)
-      uni.showToast({
-        title: error,
-        icon: 'error'
-      })
-      reject(error)
     })
-  })
 }
 
 export default {
-  baseUrl,
-  axios
+    baseUrl,
+    axios
 }
