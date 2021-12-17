9c659acfe1e082243bd039bc5042f1d96a32ce5e
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-12-17 18:51:20 +0800

    Update src\pages\recruitDetails\recruitDetails.vue

--- src\pages\recruitDetails\recruitDetails.vue
+++ src\pages\recruitDetails\recruitDetails.vue
@@ -5,29 +5,41 @@
       back-icon-color="#fff"
       title="招聘详情"
       title-color="#fff"
-      :background="{backgroundImage:'linear-gradient(to right bottom,#46e3c4,#3cc8c9)'}"
+      :background="{
+        backgroundImage: 'linear-gradient(to right bottom,#46e3c4,#3cc8c9)',
+      }"
     ></u-navbar>
- 
-    <view class="content" >
+
+    <view class="content">
+      <div class="fw600 f18 mb10 mt5">{{ newsDetails.companyname }}</div>
       <view class="flexBetween">
-              <view class="reTitle">
-                {{newsDetails.name}}
-                <u-tag class="ml10" text="急聘" mode="dark" bg-color="#fe1a19"></u-tag>
-              </view>
-              <view class="colorBlue fw600 mt5">{{newsDetails.salary}}</view>
-          </view>
-      <view class="textOver2 mt5 color3">{{newsDetails.address}}</view>
+        <view class="reTitle fw600 f16">
+          {{ newsDetails.name }}
+          <u-tag
+            class="ml10"
+            text="急聘"
+            mode="dark"
+            bg-color="#fe1a19"
+          ></u-tag>
+        </view>
+        <view class="colorBlue fw600 mt5" v-if="newsDetails.salary">{{ newsDetails.salary }}</view>
+      </view>
+      <view class="textOver2 mt5 color3" v-if="newsDetails.address">工作地址：{{ newsDetails.address }}</view>
       <view class="flexBetween mt5 mb10">
-        <view class="color2">{{newsDetails.education}}</view>
-        <view class="color2">{{newsDetails.time}}</view>
+        <view class="color2" v-if="newsDetails.education"> 学历要求：{{ newsDetails.education }}</view>
+        <view class="color2">{{ newsDetails.time }}</view>
       </view>
       <u-line class="mt10"></u-line>
-      <view class="detailsText fw600 mb10 mt10" >新闻详情</view>
+      <view class="detailsText fw600 mb10 mt10">招聘详情</view>
       <!-- <view v-html="newsDetails.details"></view> -->
       <view class="detail-content">
-        <u-parse :html="newsDetails.details" :domain="$URL" :lazy-load="true" :show-with-animation="true"></u-parse>
+        <u-parse
+          :html="newsDetails.details"
+          :domain="$URL"
+          :lazy-load="true"
+          :show-with-animation="true"
+        ></u-parse>
       </view>
-      
     </view>
   </view>
 </template>
@@ -37,19 +49,19 @@ export default {
   data() {
     return {
       //新闻列表
-      newsDetails: {}
-    }
+      newsDetails: {},
+    };
   },
-  onLoad(option){
-    this.newsDetails = JSON.parse(option.data)
+  onLoad(option) {
+    this.newsDetails = JSON.parse(option.data);
   },
-  methods: {}
-}
+  methods: {},
+};
 </script>
 
 <style lang='scss' scoped>
 .detailsText {
-  border-left: 3px solid #11BBB8;
+  border-left: 3px solid #11bbb8;
   padding-left: 5px;
 }
 .search {
