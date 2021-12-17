d75e93117fa9abc62fbdd613a63593a096e40e7f
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-12-17 18:50:51 +0800

    Update src\pages\news\news.vue

--- src\pages\news\news.vue
+++ src\pages\news\news.vue
@@ -1,17 +1,23 @@
 <template>
-  <view class="page" style="height:calc(100vh + 10px)">
+  <view class="page" style="height: calc(100vh + 10px)">
     <u-navbar
       back-txt="返回"
       back-icon-color="#fff"
       title="新闻"
       title-color="#fff"
-      :background="{backgroundImage:'linear-gradient(to right bottom,#46e3c4,#3cc8c9)'}"
+      :background="{
+        backgroundImage: 'linear-gradient(to right bottom,#46e3c4,#3cc8c9)',
+      }"
     ></u-navbar>
     <!-- 轮播图 -->
     <u-swiper :list="swiperList" @click="swiperChange"></u-swiper>
     <view style>
       <!-- 招聘列表 -->
-      <newsList class="mt10" :newsList="newsList" :status="loadStatus"></newsList>
+      <newsList
+        class="mt10"
+        :newsList="newsList"
+        :status="loadStatus"
+      ></newsList>
     </view>
   </view>
 </template>
@@ -21,61 +27,64 @@ export default {
   data() {
     return {
       swiperList: [],
-      type: '',
+      type: "",
       //招聘列表
       newsList: [],
-      loadStatus: 'loadmore', //loadmore 加载前, loading 加载中, momore 没有数据了
+      loadStatus: "loadmore", //loadmore 加载前, loading 加载中, momore 没有数据了
       find: {
         currentPage: 1, //当前页码
-        limit: 10 //每一页的数量
-      }
-    }
+        limit: 10, //每一页的数量
+      },
+    };
   },
   onLoad() {
-    this.type = 'news'
-    this.getImg()
-    this.getNewsList() // 获取招聘列表
+    this.type = "news";
+    this.getImg();
+    this.getNewsList(); // 获取招聘列表
   },
-  onReachBottom() {// 监听上拉加载
-    this.loadStatus = 'loading'
-    this.getNewsList()
+  onReachBottom() {
+    // 监听上拉加载
+    this.loadStatus = "loading";
+    this.find.currentPage++;
+    this.getNewsList();
   },
   methods: {
     getImg() {
-      this.$axios.post(this.$api.findScrollImg, 'post').then(res => {
-        this.swiperList = []
-        let arr = res.data
-        arr.map(item => {
+      this.$axios.post(this.$api.findScrollImg, "post").then((res) => {
+        this.swiperList = [];
+        let arr = res.data;
+        arr.map((item) => {
           if (item.typeid == this.type) {
-            var obj = { image: '', path: '' }
-            obj.image = item.scroimg
-            obj.path = item.link
-            this.swiperList.push(obj)
+            var obj = { image: "", path: "" };
+            obj.image = item.scroimg;
+            obj.path = item.link;
+            this.swiperList.push(obj);
           }
-        })
-      })
+        });
+      });
     },
     getNewsList() {
       var data = {
         skip: this.find.limit * (this.find.currentPage - 1),
-        limit: this.find.limit
-      }
-      this.$axios.post(this.$api.findNews, data).then(res => {
-        let arr = res.data[0].data
-        if (arr.length == 0) {
-          this.loadStatus = 'momore'
+        limit: this.find.limit,
+      };
+      this.$axios.post(this.$api.findNews, data).then((res) => {
+        if (res.code == 200) {
+          let arr = res.data[0].data
+          if (arr.length == 0) {
+            this.loadStatus = 'momore'
+            this.find.currentPage--
+          } else {
+            this.loadStatus = 'loadmore'
+            this.newsList = this.newsList.concat(arr)
+          }
         } else {
-          this.loadStatus = 'loadmore'
-          this.find.currentPage++
-          arr.map(item => {
-            item.time = item.time.split(' ')[0]
-          })
-          this.newsList = this.newsList.concat(arr)
+          this.find.currentPage--
         }
-      })
-    }
-  }
-}
+      });
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
