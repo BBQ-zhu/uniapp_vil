4390255543ee4d052d811fb15582b88484945da6
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-12-17 18:42:01 +0800

    Update src\pages\findProdect\findProdect.vue

--- src\pages\findProdect\findProdect.vue
+++ src\pages\findProdect\findProdect.vue
@@ -5,7 +5,9 @@
       back-icon-color="#fff"
       title="产品查询"
       title-color="#fff"
-      :background="{backgroundImage:'linear-gradient(to right bottom,#46e3c4,#3cc8c9)'}"
+      :background="{
+        backgroundImage: 'linear-gradient(to right bottom,#46e3c4,#3cc8c9)',
+      }"
     ></u-navbar>
     <view class="content">
       <u-search
@@ -16,7 +18,11 @@
         class="search"
       ></u-search>
       <!-- 产品服务列表 -->
-      <recommend style="margin-top:60px" :recomList="recomList" :status="loadStatus"></recommend>
+      <recommend
+        style="margin-top: 60px"
+        :recomList="recomList"
+        :status="loadStatus"
+      ></recommend>
     </view>
   </view>
 </template>
@@ -24,53 +30,61 @@
 <script>
 export default {
   data() {
-    return { 
+    return {
       find: {
         currentPage: 1, //当前页码
-        limit: 10 //每一页的数量
+        limit: 10, //每一页的数量
       },
-      search: '',
-      fuzz:'name',
+      search: "",
+      fuzz: "name",
       //产品列表
       recomList: [],
-      loadStatus: 'loadmore' //loadmore 加载前, loading 加载中, momore 没有数据了
-    }
+      loadStatus: "loadmore", //loadmore 加载前, loading 加载中, momore 没有数据了
+    };
   },
   onReachBottom() {
     // 监听上拉加载
-    this.loadStatus = 'loading'
-    this.searchChange()
+    this.loadStatus = "loading";
+    this.find.currentPage++
+    this.searchChange();
   },
   onLoad(option) {
-    this.search = option.name
-    this.fuzz = option.fuzz
-    this.searchChange()
+    this.search = option.name;
+    this.fuzz = option.fuzz;
+    this.searchChange();
   },
   methods: {
-    customFind(){
+    customFind() {
       this.find.currentPage = 1;
-      this.recomList = []
-      this.loadStatus = 'loadmore'
-      this.fuzz = 'name'
-      this.searchChange()
+      this.recomList = [];
+      this.loadStatus = "loadmore";
+      this.fuzz = "name";
+      this.searchChange();
     },
     async searchChange() {
       var data = {
         skip: this.find.limit * (this.find.currentPage - 1),
         limit: this.find.limit,
         fuzz: this.fuzz,
-        input: this.search
-      }
-      await this.$axios.post(this.$api.findProduct, data).then(res => {
-        this.recomList = this.recomList.concat(res.data[0].data)
-        this.find.currentPage++
-        if(res.data[0].data.length<=0){
-          this.loadStatus = 'momore'
+        input: this.search,
+      };
+      await this.$axios.post(this.$api.findProduct, data).then((res) => {
+        if (res.code == 200) {
+          let arr = res.data[0].data
+          if (arr.length == 0) {
+            this.loadStatus = 'momore'
+            this.find.currentPage--
+          } else {
+            this.loadStatus = 'loadmore'
+            this.recomList = this.recomList.concat(arr)
+          }
+        } else {
+          this.find.currentPage--
         }
-      })
-    }
-  }
-}
+      });
+    },
+  },
+};
 </script>
 
 <style lang='scss' scoped>
