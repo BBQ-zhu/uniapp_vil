e4e5d18d1b9e79f6276ae26257a82563e42e6910
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-12-23 16:08:31 +0800

    Update src\components\institutions\institutions.vue

--- src\components\institutions\institutions.vue
+++ src\components\institutions\institutions.vue
@@ -3,17 +3,16 @@
     <view class="u-text-center" style="color: #b7b7b7">合作伙伴</view>
     <view class="flexCenter mt10">
     <img
-    v-for="(item,index) in swiperList"
-    :key="index+'ins'"
-    :src="item.image"
-    mode="aspectFit"
-    style="width:84px;height:36px;"
-    class="mt10 mr5 ml5"
+      v-for="(item,index) in swiperList"
+      :key="index+'ins'"
+      :src="item.image"
+      mode="aspectFit"
+      style="width:75px;height:32px;"
+      class="mt10 mr5 ml5"
     />
     </view>
   </view>
 </template>
-
 <script>
 export default {
   data() {
@@ -39,7 +38,6 @@ export default {
         })
         // this.swiperList = this.swiperList.concat(this.swiperList)
       })
-
     }
   }
 }
