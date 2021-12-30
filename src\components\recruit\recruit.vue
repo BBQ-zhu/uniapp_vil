95e411530377d9f99cda9b6b5836864c4c41e4c4
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-12-30 16:41:34 +0800

    Update src\components\recruit\recruit.vue

--- src\components\recruit\recruit.vue
+++ src\components\recruit\recruit.vue
@@ -3,50 +3,63 @@
     <view class="flexBetween mb15">
       <view class="conName">招聘信息</view>
     </view>
-    <view v-for="(item,index) in recruitList" :key="index+'re'" @click="reChange(item)">
-      <view class="flexStart" style="align-items: center;">
+    <view
+      v-for="(item, index) in recruitList"
+      :key="index + 're'"
+      @click="reChange(item)"
+    >
+      <view class="flexStart" style="align-items: center">
         <view class="newName greenBtn border5 flexAlignCenter">
           <view>人</view>
           <view class="ml10">才</view>
           <view>招</view>
           <view class="ml10">聘</view>
         </view>
-        <view class="ml10" style="width:calc(100% - 100px)">
+        <view class="ml10" style="width: calc(100% - 100px)">
           <view class="flexBetween">
-              <view class="reTitle">
-                {{item.name}}
-                <u-tag class="ml10" text="急聘" mode="dark" bg-color="#fe1a19"></u-tag>
-              </view>
-              <view class="colorBlue fw600">{{item.salary}}</view>
+            <view class="reTitle">
+              {{ item.name }}
+              <u-tag
+                class="ml10"
+                text="急聘"
+                mode="dark"
+                bg-color="#fe1a19"
+              ></u-tag>
+            </view>
+            <view class="colorBlue fw600">{{ item.salary }}</view>
           </view>
-          <view class="textOver1 mt5 color3">{{item.address}}</view>
+          <view class="textOver1 mt5 color3">{{ item.address }}</view>
           <view class="flexBetween mt5">
-            <view class="color2">{{item.education}}</view>
-            <view class="color2">{{item.time}}</view>
+            <view class="color2">{{ item.education }}</view>
+            <view class="color2">{{ item.time }}</view>
           </view>
         </view>
       </view>
-      <u-line style="margin:15px 0"></u-line>
+      <u-line style="margin: 15px 0"></u-line>
     </view>
-    <u-loadmore :status="status" />
+    <view @click="$emit('reachBottom')">
+      <u-loadmore :status="status" />
+    </view>
   </view>
 </template>
 
 <script>
 export default {
-  props: ['name', 'recruitList', 'status'],
+  props: ["name", "recruitList", "status"],
   data() {
-    return {}
+    return {};
   },
   methods: {
     reChange(item) {
-      console.log(item)
+      console.log(item);
       uni.navigateTo({
-        url: '/pages/recruitDetails/recruitDetails?data=' + encodeURIComponent(JSON.stringify(item))
-      })
-    }
-  }
-}
+        url:
+          "/pages/recruitDetails/recruitDetails?data=" +
+          encodeURIComponent(JSON.stringify(item)),
+      });
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
